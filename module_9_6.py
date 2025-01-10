a652c8f6863330016a3a1ec156e8b05adaf4c9a0
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2025-01-10 22:38:22 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,8 +1,9 @@
 def all_variants(text):
   word_line = len(text)
-  for k in range(word_line):
-      for j in range(k, word_line):
-          yield text[k:j+1]
+  word_list = [text[k:j+1] for k in range(word_line) for j in range(k, word_line)]
+  word_list.sort(key=lambda item:(len(item), item))
+  for word in word_list:
+      yield word
 
 
 a = all_variants("abс")
